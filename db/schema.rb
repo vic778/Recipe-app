a30ad2c14dc561796d360b04c2839051491e7d48
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-26 16:48:59 +0300

    Create food table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_26_083052) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_26_103903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "measurement_unit"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -37,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_26_083052) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
 end
