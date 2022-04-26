b2b45f46e87bfc9485fbb4911e060b119d15ca87
SadiQ HabiL <kingqabil@live.com>
2022-04-26 21:08:30 +0300

    Fixed linters

--- db/migrate/20220426180224_create_recipe_foods.rb
+++ db/migrate/20220426180224_create_recipe_foods.rb
@@ -1,8 +1,5 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
-    create_table :recipe_foods do |t|
-
-      t.timestamps
-    end
+    create_table :recipe_foods, &:timestamps
   end
 end
