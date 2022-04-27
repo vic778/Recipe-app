1e7f355559ffc82e6f495bdfca2923a135d2cebf
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-27 13:17:34 +0300

    Fix linters errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,10 @@
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new
 
-     if user.role == 'admin'
+    if user.role == 'admin'
       can :manage, :all
     else
       # can :manage, Recipe, user_id: user.id
