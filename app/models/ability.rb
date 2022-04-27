d2d2b2ea06153b3cbea24d654abc95cc1030d07a
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-27 13:16:24 +0300

    Make correction in the abylity model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,18 @@
+
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new
 
-    if user.is? :admin
+     if user.role == 'admin'
       can :manage, :all
     else
-      can :manage, Recipe, user_id: user.id
+      # can :manage, Recipe, user_id: user.id
       can :manage, Food, user_id: user.id
-      can :manage, Recipe do |recipe|
-        recipe.user == user || recipe.public == true || !recipe.user_id?
-      end
+      # can :manage, Recipe do |recipe|
+      #   recipe.user == user || recipe.public == true || !recipe.user_id?
+      # end
     end
   end
 end
